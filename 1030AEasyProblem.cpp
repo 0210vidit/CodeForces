d746d3968680c871eed591c6389dfb1a94637263
Vidit Shah <0210vidit@gmail.com>
2020-06-26 00:34:27 +0530

    Easy

---
+++ 1030AEasyProblem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    int temp;
+    cin >> temp;
+    if (temp == 1)
+    {
+      cout << "HARD" << endl;
+      exit(0);
+    }
+  }
+  cout << "EASY";
+}
